93c9b6fb1bebeda21056b2cae28df892dc02c6e9
tan vu <vtan01.04@gmail.com>
2021-11-03 00:42:05 +0700

    bài 25+26: Cookie Authentication without ASP.NET Identity

---
+++ TTC-ShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TTC_ShopSolution.ViewModels.System.Users;
+
+namespace TTC_ShopSolution.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
